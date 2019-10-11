7d75d0ce4cda2b90fccf52d7f009988c5dd4576c
liyao5 <liyao5@staff.sina.com.cn>
2019-10-11 14:49:15 +0800

    add test for post

---
+++ ly_posts.lua
@@ -0,0 +1,11 @@
+local _M = {}
+
+local posts = {
+"http://multiroot.sinaedge.com/primary/test/index.html",
+"http://multiroot.sinaedge.com/primary/test/index2.html",
+"http://multiroot.sinaedge.com/50x.html",
+}
+
+_M.posts = posts
+
+return _M
--- scripts/ly_setup.lua
+++ scripts/ly_setup.lua
@@ -4,7 +4,6 @@
 
 local counter = 1
 local threads = {}
-local query = require "args"
 
 -- 创建线程
 function setup(thread)
@@ -23,13 +22,29 @@ function init(args)
    print(msg:format(id))
 end
 
--- 修改请求参数
+-- 修改请求参数,测试不同post,不同header,注意此函数的参数 wrk.format(method, path, headers, body)。在文件src/wrk.lua
+local posts = require "ly_posts"
 function request()
    requests = requests + 1
-   local path = query.args[math.random(0,57)]
-   return wrk.format(nil,path)
-    -- return wrk.request()
+   local path = "/uri_del"
+   local headers = {}
+   local body = posts.posts[math.random(1,3)] 
+   headers["Host"] = "uri-del.sinaedge.com"
+   headers["uriid"] = math.random(0,100)
+   if requests%2 == 1 then
+        headers["uri-action"] = "recover"
+   end
+   return wrk.format("POST", path, headers, body)
 end
+
+-- 修改请求参数,猜测试不同query
+--local query = require "ly_args"
+--function request()
+--   requests = requests + 1
+--   local path = query.args[math.random(0,57)]
+--   return wrk.format(nil,path)
+--    -- return wrk.request()
+--end
 
 -- 读取结果
 function response(status, headers, body)
